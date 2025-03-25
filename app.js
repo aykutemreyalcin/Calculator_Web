8c2053503d0869abf751300ea5daa20e27291093
aykutemreyalcin <aykutemyeyalcinn@gmail.com>
2025-03-25 21:57:40 +0100

    finishing js

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const display = document.getElementById('display');
 
 function append_to_display(input) {
-    display.append(input);
+    display.value += input;
 }
 
 function clear_display() {
     display.value = ""
+}
+
+function calculate() {
+    try {
+        display.value = (eval(display.value)).toFixed(2)
+    } 
+    catch (error) {
+        display.value = "Error";
+    }
+}
+
+function reverse() {
+    display.value = -(display.value)
 }
